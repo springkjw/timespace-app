8edeaf4fa9e10f594e99dba32001a87b82ae5622
JaewonKwon <springkjw@gmail.com>
2020-02-23 18:36:47 +0900

    안드로이드 카카오 로그인

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,9 +1,17 @@
 import React, {useState} from 'react';
+import KakaoLogins from '@react-native-seoul/kakao-login';
 import View from './view';
 
 export default () => {
-  const onKakao = () => {
-
+  const onKakao = async () => {
+    if (KakaoLogins) {
+      try {
+        const result = await KakaoLogins.login();
+        console.log(result);
+      } catch (err) {
+        console.log(err);
+      }
+    }
   };
 
   const onGoogle = () => {
